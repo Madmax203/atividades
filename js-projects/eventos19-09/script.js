2ce526db14213fe9aa4f46dc747cf7f9d7cb0211
Madmax203 <maxj644@gmail.com>
2026-09-22 17:20:31 -0300

    tentativa de fazer a mascara na tela de login

--- js-projects/eventos19-09/script.js
+++ js-projects/eventos19-09/script.js
@@ -24,7 +24,35 @@ btnSubtract.onclick = function() {
 
 
 
-// section 2 //
+// section 2 mouseover mouseout //
 
+let area = document.getElementById("areaMouse")
+let mensagem = document.getElementById("resultadoMouse")
 
+area.onmouseover = function() {
+    area.style.backgroundColor = 'azure'
+    area.style.color = 'orange'
+    area.style.fontWeight = 'bold'
+    mensagem.innerHTML = "O mouse entrou na área!!!"
+}
 
+area.onmouseout = function() {
+    area.style.backgroundColor = ''
+    area.style.color = ''
+    area.style.fontWeight = ''
+    mensagem.innerHTML = ''
+
+}
+
+// Section 3 keydown //
+
+let entrada = document.querySelector("#campoTeclado")
+let display = document.querySelector("#resultadoTeclado")
+
+entrada.addEventListener("keydown", function(event){
+    display.innerHTML = event.key
+})
+
+// Section 4 change //
+
+let selecao = document.querySelector("#selecao")
